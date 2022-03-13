a5a63b231a075f86073a92864e64dabc6d5be43d
Valeriy <valeriy.gnybidenko@gmail.com>
2022-03-13 12:45:18 +0200

    change inputs without fixed

--- components/AppInput.vue
+++ components/AppInput.vue
@@ -10,15 +10,15 @@
     />
     <div v-if="isSearch" class="search-input">
       <svg
-        width="20"
-        height="20"
+        width="15.6"
+        height="15.6"
         viewBox="0 0 20 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
           d="M19 19L13 13M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8Z"
-          stroke="#333333"
+          stroke="#F1F1F199"
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
