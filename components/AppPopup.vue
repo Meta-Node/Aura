62149c7d68e1abb29fb08863a857321abd0a153f
Valeriy <valeriy.gnybidenko@gmail.com>
2022-04-01 11:03:10 +0300

    fix popup

---
+++ components/AppPopup.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="popup" :class="[isOpen && 'popup--open']">
+    <div class="popup__backdrop"></div>
+    <div class="popup__window">
+      <button class="popup__close" @click="closePopup">
+        <span class="popup__close-line"></span>
+        <span class="popup__close-line"></span>
+      </button>
+      <slot> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    openPopup() {
+      this.isOpen = true
+    },
+    closePopup() {
+      this.isOpen = false
+    },
+  },
+}
+</script>
