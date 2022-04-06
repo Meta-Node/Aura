ce4fffc85ecdfe0ce4788d77019d5255f5851c81
letmewin22 <lkosteckiy5@gmail.com>
2022-04-06 23:03:07 +0300

    add setNickname service

--- scripts/api/connections.service.js
+++ scripts/api/connections.service.js
@@ -1,7 +1,6 @@
-import { backendApi } from '.'
+import { backendApi, encryptData } from '.'
 
 export const getConnections = async fromBrightId => {
-  // search
   const res = await backendApi.get('/v1/connections/search', {
     fromBrightId,
     seed: 5,
@@ -11,7 +10,21 @@ export const getConnections = async fromBrightId => {
 }
 
 export const getProfile = async fromBrightId => {
-  // search
   const res = await backendApi.get('/v1/profile/' + fromBrightId)
+  return res
+}
+
+export const setNickname = async ({ fromBrightId, toBrightId, nickname }) => {
+  const endpoint = '/v1/nickname/'
+  const URL = `${endpoint}${fromBrightId}/${toBrightId}`
+
+  const encryptedData = {
+    nickname,
+  }
+
+  const encryptedNickname = encryptData(encryptedData)
+  const res = await backendApi.post(URL, {
+    encryptedNickname,
+  })
   return res
 }
