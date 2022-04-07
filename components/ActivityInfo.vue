5761b62403a12396fed22e7d8390d8da80d636b2
Valeriy <valeriy.gnybidenko@gmail.com>
2022-04-07 09:28:07 +0300

    fix activity-info

--- components/ActivityInfo.vue
+++ components/ActivityInfo.vue
@@ -1,23 +1,27 @@
 <template>
   <li class="activity__list">
-    <div class="activity__info-block">
-      <toggle-switch :id="id" />
-      <div class="activity__text">
-        <span v-if="fromUser" class="activity__from-user"
-          ><nuxt-link :to="'/profile/' + fromUser.id">{{
-            fromUser.name
-          }}</nuxt-link></span
-        >
-        <span class="activity__action">{{
-          action.action + ' ' + action.amount
-        }}</span>
-        <span v-if="toUser" class="activity__to-user"
-          ><nuxt-link :to="'/profile/' + toUser.id">{{
-            toUser.name
-          }}</nuxt-link></span
-        >
+    <div class="activity__main">
+      <div class="activity__left-block">
+        <toggle-switch :id="id" />
       </div>
-      <p class="activity__time">{{ computeDate(time) }}</p>
+      <div class="activity__right-block">
+        <div class="activity__text">
+          <span v-if="fromUser" class="activity__from-user"
+            ><nuxt-link :to="'/profile/' + fromUser.id">{{
+              fromUser.name
+            }}</nuxt-link></span
+          >
+          <span class="activity__action">{{
+            action.action + ' ' + action.amount
+          }}</span>
+          <span v-if="toUser" class="activity__to-user"
+            ><nuxt-link :to="'/profile/' + toUser.id">{{
+              toUser.name
+            }}</nuxt-link></span
+          >
+        </div>
+        <p class="activity__time">{{ computeDate(time) }}</p>
+      </div>
     </div>
     <div class="activity__stripe"></div>
   </li>
