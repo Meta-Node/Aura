0f72f6903885d27d2462b09dad349a1bfb8ffa68
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-07-21 00:38:36 +0430

    fix: dotenv file config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+const DOTENV_PATH =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+require('dotenv').config({ path: DOTENV_PATH })
 
 const title = 'Discover & grow your Aura...'
 const description = 'Your Aura represents power & influence upon the Auracle.'
@@ -81,7 +83,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
-    ['@nuxtjs/dotenv', { filename: `.env.${process.env.NODE_ENV}` }],
+    ['@nuxtjs/dotenv', { filename: DOTENV_PATH }],
     '@nuxtjs/pwa',
     '@nuxtjs/axios',
   ],
