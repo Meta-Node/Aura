d38d53f7224039be65ecfe1067dbd1f719452a1e
Ali Mahdiyar <alimahdiyar77@gmail.com>
2022-08-17 18:14:12 +0430

    feat: add profile explorer link

--- components/profile/PublicProfile.vue
+++ components/profile/PublicProfile.vue
@@ -1,25 +1,26 @@
 <template>
   <section class="profile">
     <div v-if="isLoading" style="margin-top: 40px">
-      <app-spinner :is-visible="true" />
+      <app-spinner :is-visible="true"/>
     </div>
     <div v-else-if="!isLoading && !profile" class="container">
       User not found
     </div>
     <div v-else class="container profile__wrapper">
       <profile-info
+        :id="profile.id"
+        :brightness="brightness"
+        :connection-date="profile.connectionDate"
+        :connections="profile.numOfConnections"
+        :date="date"
         :img="profileAvatar"
+        :is-own-profile="isOwn"
         :name="profile.name || 'Unknown'"
         :rating="profile.rating"
-        :date="date"
-        :connections="profile.numOfConnections"
-        :brightness="brightness"
-        :is-own-profile="isOwn"
-        :connectionDate="profile.connectionDate"
         @share="onShare"
       />
-      <aura-sphere class="profile__sphere" :rating="profile.rating" />
-      <four-unrated :users="fourUnrated" />
+      <aura-sphere :rating="profile.rating" class="profile__sphere"/>
+      <four-unrated :users="fourUnrated"/>
     </div>
   </section>
 </template>
@@ -33,16 +34,18 @@ import avatar from '~/mixins/avatar'
 import FourUnrated from '~/components/FourUnrated.vue'
 
 export default {
-  components: { ProfileInfo, AuraSphere, FourUnrated },
+  components: {ProfileInfo, AuraSphere, FourUnrated},
   mixins: [transition, avatar],
   props: {
     profile: {
       type: Object,
-      default: () => {},
+      default: () => {
+      },
     },
     connections: {
       type: Object,
-      default: () => {},
+      default: () => {
+      },
     },
     isLoading: {
       type: Boolean,
